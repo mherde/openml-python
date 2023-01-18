cb9148ca405657d0bf37d55ccacd1a8fbf359f51
tuan <tuanphamminh93@gmail.com>
2023-01-18 17:40:29 +0100

    Add test notebook and implement Active Learning cycle

--- openml/extensions/skactiveml/extension.py
+++ openml/extensions/skactiveml/extension.py
@@ -1,8 +1,46 @@
+from openml.tasks.task import TaskType
 from ..sklearn import SklearnExtension
 from openml.flows import OpenMLFlow
 from typing import Any, Callable, Dict, List, Optional, Set, Tuple, Union, cast, Sized
 from skactiveml.base import SingleAnnotatorPoolQueryStrategy, SkactivemlClassifier
 from collections import OrderedDict
+
+from collections import OrderedDict  # noqa: F401
+import copy
+from distutils.version import LooseVersion
+import importlib
+import inspect
+import json
+import logging
+import re
+from re import IGNORECASE
+import sys
+import time
+from typing import Any, Callable, Dict, List, Optional, Set, Tuple, Union, cast, Sized
+import warnings
+
+import numpy as np
+import pandas as pd
+import scipy.stats
+import scipy.sparse
+import sklearn.base
+import sklearn.model_selection
+import sklearn.pipeline
+
+import openml
+from openml.exceptions import PyOpenMLError
+from openml.extensions import Extension
+from openml.flows import OpenMLFlow
+from openml.runs.trace import OpenMLRunTrace, OpenMLTraceIteration, PREFIX
+from openml.tasks import (
+    OpenMLTask,
+    OpenMLSupervisedTask,
+    OpenMLClassificationTask,
+    OpenMLLearningCurveTask,
+    OpenMLClusteringTask,
+    OpenMLRegressionTask,
+    OpenMLActiveClassificationTask
+)
 
 
 class SkactivemlExtension(SklearnExtension):
@@ -23,7 +61,7 @@ class SkactivemlExtension(SklearnExtension):
         return cls._is_skactiveml_flow(flow)
 
     @classmethod
-    def _is_sklearn_flow(cls, flow: OpenMLFlow) -> bool:
+    def _is_skactiveml_flow(cls, flow: OpenMLFlow) -> bool:
         if getattr(flow, "dependencies", None) is not None and "skactiveml" in flow.dependencies:
             return True
         if flow.external_version is None:
@@ -72,9 +110,9 @@ class SkactivemlExtension(SklearnExtension):
 
         flow.model = OrderedDict()
 
-        flow.model['query_strategy'] = self._serialize_sklearn(model['query_strategy'])
-        flow.model['prediction_model'] = self._serialize_sklearn(model['prediction_model'])
-        flow.model['selector_model'] = self._serialize_sklearn(model['selector_model'])
+        flow.model['query_strategy'] = model['query_strategy']
+        flow.model['prediction_model'] = model['prediction_model']
+        flow.model['selector_model'] = model['selector_model']
         return flow
 
     def is_estimator(self, model: Any) -> bool:
@@ -93,7 +131,7 @@ class SkactivemlExtension(SklearnExtension):
         """
         o = model
         return (hasattr(o, "fit") or hasattr(o, "query")) and hasattr(o, "get_params") and hasattr(o, "set_params")
-    
+
     def check_if_model_fitted(self, model: Any) -> bool:
         """Returns True/False denoting if the model has already been fitted/trained
 
@@ -150,5 +188,245 @@ class SkactivemlExtension(SklearnExtension):
         """
 
         for k, v in model.items():
-            model[k].model = super().seed_model(v.model, seed)
+            model[k].model = super().seed_model(v, seed)
         return model
+
+    def _run_model_on_fold(
+        self,
+        model: Any,
+        task: "OpenMLTask",
+        X_train: Union[np.ndarray, scipy.sparse.spmatrix, pd.DataFrame],
+        rep_no: int,
+        fold_no: int,
+        y_train: Optional[np.ndarray] = None,
+        X_test: Optional[Union[np.ndarray, scipy.sparse.spmatrix, pd.DataFrame]] = None,
+    ) -> Tuple[
+        np.ndarray, Optional[pd.DataFrame], "OrderedDict[str, float]", Optional[OpenMLRunTrace]
+    ]:
+        """Run a model on a repeat,fold,subsample triplet of the task and return prediction
+        information.
+
+        Furthermore, it will measure run time measures in case multi-core behaviour allows this.
+        * exact user cpu time will be measured if the number of cores is set (recursive throughout
+        the model) exactly to 1
+        * wall clock time will be measured if the number of cores is set (recursive throughout the
+        model) to any given number (but not when it is set to -1)
+
+        Returns the data that is necessary to construct the OpenML Run object. Is used by
+        run_task_get_arff_content. Do not use this function unless you know what you are doing.
+
+        Parameters
+        ----------
+        model : Any
+            The UNTRAINED model to run. The model instance will be copied and not altered.
+        task : OpenMLTask
+            The task to run the model on.
+        X_train : array-like
+            Training data for the given repetition and fold.
+        rep_no : int
+            The repeat of the experiment (0-based; in case of 1 time CV, always 0)
+        fold_no : int
+            The fold nr of the experiment (0-based; in case of holdout, always 0)
+        y_train : Optional[np.ndarray] (default=None)
+            Target attributes for supervised tasks. In case of classification, these are integer
+            indices to the potential classes specified by dataset.
+        X_test : Optional, array-like (default=None)
+            Test attributes to test for generalization in supervised tasks.
+
+        Returns
+        -------
+        pred_y : np.ndarray
+            Predictions on the training/test set, depending on the task type.
+            For supervised tasks, predictions are on the test set.
+            For unsupervised tasks, predictions are on the training set.
+        proba_y : pd.DataFrame, optional
+            Predicted probabilities for the test set.
+            None, if task is not Classification or Learning Curve prediction.
+        user_defined_measures : OrderedDict[str, float]
+            User defined measures that were generated on this fold
+        trace : OpenMLRunTrace, optional
+            arff trace object from a fitted model and the trace content obtained by
+            repeatedly calling ``run_model_on_task``
+        """
+
+        def _prediction_to_probabilities(
+            y: Union[np.ndarray, List], model_classes: List[Any], class_labels: Optional[List[str]]
+        ) -> pd.DataFrame:
+            """Transforms predicted probabilities to match with OpenML class indices.
+
+            Parameters
+            ----------
+            y : np.ndarray
+                Predicted probabilities (possibly omitting classes if they were not present in the
+                training data).
+            model_classes : list
+                List of classes known_predicted by the model, ordered by their index.
+            class_labels : list
+                List of classes as stored in the task object fetched from server.
+
+            Returns
+            -------
+            pd.DataFrame
+            """
+            if class_labels is None:
+                raise ValueError("The task has no class labels")
+
+            if isinstance(y_train, np.ndarray) and isinstance(class_labels[0], str):
+                # mapping (decoding) the predictions to the categories
+                # creating a separate copy to not change the expected pred_y type
+                y = [class_labels[pred] for pred in y]  # list or numpy array of predictions
+
+            # model_classes: sklearn classifier mapping from original array id to
+            # prediction index id
+            if not isinstance(model_classes, list):
+                raise ValueError("please convert model classes to list prior to calling this fn")
+
+            # DataFrame allows more accurate mapping of classes as column names
+            result = pd.DataFrame(
+                0, index=np.arange(len(y)), columns=model_classes, dtype=np.float32
+            )
+            for obs, prediction in enumerate(y):
+                result.loc[obs, prediction] = 1.0
+            return result
+
+        query_strategy = sklearn.base.clone(model['query_strategy'])
+        prediction_model = sklearn.base.clone(model['prediction_model'])
+        selection_model = sklearn.base.clone(model['selector_model'])
+
+        if isinstance(task, OpenMLSupervisedTask):
+            if y_train is None:
+                raise TypeError("argument y_train must not be of type None")
+            if X_test is None:
+                raise TypeError("argument X_test must not be of type None")
+
+        # sanity check: prohibit users from optimizing n_jobs
+        self._prevent_optimize_n_jobs(prediction_model)
+        self._prevent_optimize_n_jobs(selection_model)
+        # measures and stores runtimes
+        user_defined_measures = OrderedDict()  # type: 'OrderedDict[str, float]'
+        try:
+            # for measuring runtime. Only available since Python 3.3
+            modelfit_start_cputime = time.process_time()
+            modelfit_start_walltime = time.time()
+
+            if isinstance(task, OpenMLActiveClassificationTask):
+                y = np.full(shape=y_train.shape, fill_value=None)
+                for c in range(task.budget):
+                    query_idx = query_strategy.query(X=X_train, y=y)
+                    y[query_idx] = y_train.values[query_idx]
+                prediction_model.fit(X_train, y)
+            modelfit_dur_cputime = (time.process_time() - modelfit_start_cputime) * 1000
+            modelfit_dur_walltime = (time.time() - modelfit_start_walltime) * 1000
+
+            user_defined_measures["usercpu_time_millis_training"] = modelfit_dur_cputime
+            refit_time = prediction_model.refit_time_ * 1000 if hasattr(prediction_model, "refit_time_") else 0
+            user_defined_measures["wall_clock_time_millis_training"] = modelfit_dur_walltime
+
+        except AttributeError as e:
+            # typically happens when training a regressor on classification task
+            raise PyOpenMLError(str(e))
+
+        if isinstance(task, OpenMLActiveClassificationTask):
+            # search for model classes_ (might differ depending on modeltype)
+            # first, pipelines are a special case (these don't have a classes_
+            # object, but rather borrows it from the last step. We do this manually,
+            # because of the BaseSearch check)
+            if isinstance(prediction_model, sklearn.pipeline.Pipeline):
+                used_estimator = prediction_model.steps[-1][-1]
+            else:
+                used_estimator = prediction_model
+
+            if self._is_hpo_class(used_estimator):
+                model_classes = used_estimator.best_estimator_.classes_
+            else:
+                model_classes = used_estimator.classes_
+
+            if not isinstance(model_classes, list):
+                model_classes = model_classes.tolist()
+
+            # to handle the case when dataset is numpy and categories are encoded
+            # however the class labels stored in task are still categories
+            if isinstance(y_train, np.ndarray) and isinstance(
+                cast(List, task.class_labels)[0], str
+            ):
+                model_classes = [cast(List[str], task.class_labels)[i] for i in model_classes]
+
+        modelpredict_start_cputime = time.process_time()
+        modelpredict_start_walltime = time.time()
+
+        # In supervised learning this returns the predictions for Y, in clustering
+        # it returns the clusters
+        if isinstance(task, OpenMLActiveClassificationTask):
+            pred_y = prediction_model.predict(X_test)
+        else:
+            raise ValueError(task)
+
+        modelpredict_duration_cputime = (time.process_time() - modelpredict_start_cputime) * 1000
+        user_defined_measures["usercpu_time_millis_testing"] = modelpredict_duration_cputime
+        user_defined_measures["usercpu_time_millis"] = (
+            modelfit_dur_cputime + modelpredict_duration_cputime
+        )
+        modelpredict_duration_walltime = (time.time() - modelpredict_start_walltime) * 1000
+        user_defined_measures["wall_clock_time_millis_testing"] = modelpredict_duration_walltime
+        user_defined_measures["wall_clock_time_millis"] = (
+            modelfit_dur_walltime + modelpredict_duration_walltime + refit_time
+        )
+
+        if isinstance(task, OpenMLActiveClassificationTask):
+
+            try:
+                proba_y = prediction_model.predict_proba(X_test)
+                proba_y = pd.DataFrame(proba_y, columns=model_classes)  # handles X_test as numpy
+            except AttributeError:  # predict_proba is not available when probability=False
+                proba_y = _prediction_to_probabilities(pred_y, model_classes, task.class_labels)
+
+            if task.class_labels is not None:
+                if proba_y.shape[1] != len(task.class_labels):
+                    # Remap the probabilities in case there was a class missing
+                    # at training time. By default, the classification targets
+                    # are mapped to be zero-based indices to the actual classes.
+                    # Therefore, the model_classes contain the correct indices to
+                    # the correct probability array. Example:
+                    # classes in the dataset: 0, 1, 2, 3, 4, 5
+                    # classes in the training set: 0, 1, 2, 4, 5
+                    # then we need to add a column full of zeros into the probabilities
+                    # for class 3 because the rest of the library expects that the
+                    # probabilities are ordered the same way as the classes are ordered).
+                    message = "Estimator only predicted for {}/{} classes!".format(
+                        proba_y.shape[1],
+                        len(task.class_labels),
+                    )
+                    warnings.warn(message)
+                    openml.config.logger.warning(message)
+
+                    for i, col in enumerate(task.class_labels):
+                        # adding missing columns with 0 probability
+                        if col not in model_classes:
+                            proba_y[col] = 0
+                    # We re-order the columns to move possibly added missing columns into place.
+                    proba_y = proba_y[task.class_labels]
+            else:
+                raise ValueError("The task has no class labels")
+
+            if not np.all(set(proba_y.columns) == set(task.class_labels)):
+                missing_cols = list(set(task.class_labels) - set(proba_y.columns))
+                raise ValueError("Predicted probabilities missing for the columns: ", missing_cols)
+
+        elif isinstance(task, OpenMLRegressionTask):
+            proba_y = None
+
+        elif isinstance(task, OpenMLClusteringTask):
+            proba_y = None
+
+        else:
+            raise TypeError(type(task))
+
+        if self._is_hpo_class(prediction_model):
+            trace_data = self._extract_trace_data(prediction_model, rep_no, fold_no)
+            trace = self._obtain_arff_trace(
+                prediction_model, trace_data
+            )  # type: Optional[OpenMLRunTrace]  # noqa E501
+        else:
+            trace = None
+
+        return pred_y, proba_y, user_defined_measures, trace
